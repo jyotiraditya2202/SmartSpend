e3f6a9107e462022f55daca07bfc4b68e284c813
rjyotiraditya <rjyotiraditya0@gmail.com>
2025-08-07 18:59:50 +0530

    deploying debug

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,7 @@ const connectDB = require('../../server/src/config/db');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://smartspend-1.onrender.com', credentials: true }));
 app.use(express.json());
 
 // Middleware to parse JSON
